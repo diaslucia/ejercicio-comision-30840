49e98d07e79b204c291e6d99ed804c8a0a32617b
Lucy <lucy_17_4@hotmail.com>
2022-09-01 18:18:46 -0300

    Proyecto base

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Ejercicio de repaso
+## Tutoria 30840 📚
 
-Repaso de consumo de API y React Router DOM
+Ejercicio de repaso de consumo de API y React Router DOM
+
+### Instalación y ejecución
+
+1. Clonar el repositorio
+```
+$ git clone https://github.com/diaslucia/ejercicio-comision-30840.git
+```
+2. La rama main tiene el ejercicio vacío. Para ir al resultado:
+```
+$ git checkout terminado
+```
---
+++ src/components/MovieDetail/MovieDetail.js
@@ -0,0 +1,18 @@
+import "./MovieDetail.css";
+
+const MovieDetail = () => {
+
+    return(
+        <div className="movieDetail">
+            <div className="movieDetail_card">
+                <img/>
+                <div>
+                    <h3 className="movieDetail_title"></h3>
+                    <p className="movieDetail_overview"></p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MovieDetail;
